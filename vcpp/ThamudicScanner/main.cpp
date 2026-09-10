0a79c16e2755bd2eeb7d456b7a812bbbba747471
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-11 00:10:53 +0300

    Fix Visual C++ UTF-8 output

--- vcpp/ThamudicScanner/main.cpp
+++ vcpp/ThamudicScanner/main.cpp
@@ -1,3 +1,4 @@
 #include <iostream>
+#include <string>
 #include "thamudic/thamudic.hpp"
-int main(){std::string s;std::getline(std::cin,s);auto cps=chimera::thamudic::codePoints(std::u8string(reinterpret_cast<const char8_t*>(s.data()),s.size()));std::u32string t(cps.begin(),cps.end());std::cout<<"Thamudic code points: "<<chimera::thamudic::toUtf8(chimera::thamudic::extractThamudic(t)).c_str()<<"\n";}
+int main(){std::string s;std::getline(std::cin,s);auto cps=chimera::thamudic::codePoints(std::u8string(reinterpret_cast<const char8_t*>(s.data()),s.size()));std::u32string t(cps.begin(),cps.end());auto u=chimera::thamudic::toUtf8(chimera::thamudic::extractThamudic(t));std::cout<<"Thamudic: "<<reinterpret_cast<const char*>(u.c_str())<<"\n";}
