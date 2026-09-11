1a2e570057302f91c4311b50a52fe33a042910be
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-11 09:20:54 +0300

    Revert last FlashTool enhancement

--- FlashTool/c/flashtool.c
+++
@@ -1,5 +0,0 @@
-#include "../core/include/flashtool.h"
-
-int flashtool_safe_plan(const chm_device_info_t *device, const chm_flash_plan_t *plan) {
-    return chm_validate_flash_plan(device, plan);
-}
