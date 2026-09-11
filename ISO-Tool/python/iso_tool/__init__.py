a17976c6b177146da4c9f2d51d523d066e74edfd
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-11 10:15:20 +0300

    ISO-Tool: expose recursive repository build APIs

--- ISO-Tool/python/iso_tool/__init__.py
+++ ISO-Tool/python/iso_tool/__init__.py
@@ -2,8 +2,10 @@
 from .pipeline import BuildPipeline, BuildProgress
 from .advanced_inspect import AdvancedInspection, inspect_advanced
 from .reproducible import BuildProvenance, make_provenance
+from .recursive_build import RecursiveReport, SourceRecord, ManifestRecord, BuildArtifact, inventory, build_repository, acquire_repository
 
 __all__ = [
     "BuildPipeline", "BuildProgress", "AdvancedInspection", "inspect_advanced",
-    "BuildProvenance", "make_provenance",
+    "BuildProvenance", "make_provenance", "RecursiveReport", "SourceRecord",
+    "ManifestRecord", "BuildArtifact", "inventory", "build_repository", "acquire_repository",
 ]
