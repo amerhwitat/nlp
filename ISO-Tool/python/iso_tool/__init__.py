903edc4b025a60b0379a3c7a735c94d0aecb3532
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-11 10:21:48 +0300

    ISO-Tool: export recursive dependency graph APIs

--- ISO-Tool/python/iso_tool/__init__.py
+++ ISO-Tool/python/iso_tool/__init__.py
@@ -2,10 +2,12 @@
 from .pipeline import BuildPipeline, BuildProgress
 from .advanced_inspect import AdvancedInspection, inspect_advanced
 from .reproducible import BuildProvenance, make_provenance
+from .external_refs import ExternalReference, DependencyGraph, discover_external_references, write_dependency_report
 from .recursive_build import RecursiveReport, SourceRecord, ManifestRecord, BuildArtifact, inventory, build_repository, acquire_repository
 
 __all__ = [
     "BuildPipeline", "BuildProgress", "AdvancedInspection", "inspect_advanced",
-    "BuildProvenance", "make_provenance", "RecursiveReport", "SourceRecord",
-    "ManifestRecord", "BuildArtifact", "inventory", "build_repository", "acquire_repository",
+    "BuildProvenance", "make_provenance", "ExternalReference", "DependencyGraph",
+    "discover_external_references", "write_dependency_report", "RecursiveReport",
+    "SourceRecord", "ManifestRecord", "BuildArtifact", "inventory", "build_repository", "acquire_repository",
 ]
