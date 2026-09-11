47b21f3e92baae100d014608b786d3e574e5b092
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-11 12:55:49 +0300

    feat(iso-tool): export toolchain detector API

--- ISO-Tool/python/iso_tool/__init__.py
+++ ISO-Tool/python/iso_tool/__init__.py
@@ -1,6 +1,7 @@
 """ISO-Tool Python reference implementation."""
 from .pipeline import BuildPipeline, BuildProgress
 from .output_paths import dependency_cache_dir, prepare_output_layout, profile_downloads, suggested_output_dir
+from .toolchain_detector import detect_tools, apply_user_environment
 
 __all__ = [
     "BuildPipeline",
@@ -9,4 +10,6 @@ __all__ = [
     "prepare_output_layout",
     "profile_downloads",
     "suggested_output_dir",
+    "detect_tools",
+    "apply_user_environment",
 ]
