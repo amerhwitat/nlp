8474f04c6dfe27f8d9e67dab51ae0a4c74304a9a
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-11 10:50:07 +0300

    ISO-Tool: expose Windows toolchain discovery API

--- ISO-Tool/python/iso_tool/__init__.py
+++ ISO-Tool/python/iso_tool/__init__.py
@@ -4,10 +4,5 @@ from .advanced_inspect import AdvancedInspection, inspect_advanced
 from .reproducible import BuildProvenance, make_provenance
 from .external_refs import ExternalReference, DependencyGraph, discover_external_references, write_dependency_report
 from .recursive_build import RecursiveReport, SourceRecord, ManifestRecord, BuildArtifact, inventory, build_repository, acquire_repository
-
-__all__ = [
-    "BuildPipeline", "BuildProgress", "AdvancedInspection", "inspect_advanced",
-    "BuildProvenance", "make_provenance", "ExternalReference", "DependencyGraph",
-    "discover_external_references", "write_dependency_report", "RecursiveReport",
-    "SourceRecord", "ManifestRecord", "BuildArtifact", "inventory", "build_repository", "acquire_repository",
-]
+from .windows_toolchains import Toolchain, discover, write_report
+__all__=["BuildPipeline","BuildProgress","AdvancedInspection","inspect_advanced","BuildProvenance","make_provenance","ExternalReference","DependencyGraph","discover_external_references","write_dependency_report","RecursiveReport","SourceRecord","ManifestRecord","BuildArtifact","inventory","build_repository","acquire_repository","Toolchain","discover","write_report"]
