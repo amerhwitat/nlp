f1cb37c7d2af10b9e7c5c3cc5291cb8b8032503f
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-12 14:44:48 +0300

    build: add Perl build orchestrator

---
+++ scripts/build/build-all.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+use strict; use warnings; use File::Basename qw(dirname); use Cwd qw(abs_path); use File::Spec;
+my $root = abs_path(File::Spec->catdir(dirname(__FILE__), '..', '..'));
+sub run_step { my ($name,@cmd)=@_; print "== $name ==\n"; system(@cmd)==0 or die "$name failed\n"; }
+chdir $root or die "Cannot enter $root: $!";
+if (-f 'web/package.json') { my $npm = $^O eq 'MSWin32' ? 'npm.cmd' : 'npm'; run_step('Web install', $npm, (-f 'web/package-lock.json' ? 'ci' : 'install')) if chdir('web'); chdir($root); run_step('Web build',$npm,'run','build'); }
+run_step('Python validation,$^X','-m','compileall','-q','python') if -d 'python';
+run_step('.NET build','dotnet','build','dotnet','-c','Release','--nologo') if -d 'dotnet';
+print "Perl build orchestrator completed.\n";
